e7567774599394668ea5fb798dc9783e3e181696
mynk03 <mayank4code@gmail.com>
2025-03-11 18:22:54 +0530

    feat: add blockchain initialization with storage
    * add path for chaindata
    * add CreateBlockchain method to initialize blockchain with genesis block

--- blockchain/genesis.go
+++ blockchain/genesis.go
@@ -2,7 +2,9 @@ package blockchain
 
 import (
 	"blockchain-simulator/state"
+	"log"
 	"time"
+
 	"github.com/ethereum/go-ethereum/common"
 )
 
@@ -16,7 +18,6 @@ func CreateGenesisBlock(accountsToFund []string, amountsToFund []uint64) Block {
 		genesisAccounts[common.HexToAddress(addr)] = &state.Account{Balance: amountsToFund[i], Nonce: 0}
 	}
 
-
 	return Block{
 		Index:        0,
 		Timestamp:    time.Now().UTC().String(),
@@ -24,5 +25,46 @@ func CreateGenesisBlock(accountsToFund []string, amountsToFund []uint64) Block {
 		PrevHash:     "0",
 		Hash:         CalculateBlockHash(Block{Index: 0}), // Placeholder
 		StateRoot:    stateTrie.RootHash(),
+	}
+}
+
+// Initialize storage
+func InitializeStorage() *LevelDBStorage {
+	dbPath := "../chaindata"
+	storage, err := NewLevelDBStorage(dbPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return storage
+}
+
+// Create blockchain with storage
+func CreateBlockchain(storage *LevelDBStorage, accountsToFund []string, amountsToFund []uint64) *Blockchain {
+	// Initialize the state trie
+	stateTrie := state.NewTrie()
+
+	// Create the genesis block
+	genesisBlock := CreateGenesisBlock(accountsToFund, amountsToFund)
+
+	// Seed initial accounts into the state trie
+	genesisAccounts := map[common.Address]*state.Account{}
+	for i, addr := range accountsToFund {
+		genesisAccounts[common.HexToAddress(addr)] = &state.Account{Balance: amountsToFund[i], Nonce: 0}
+	}
+	for addr, acc := range genesisAccounts {
+		stateTrie.PutAccount(addr, acc)
+		storage.PutAccount(addr, acc)
+	}
+
+	// Store genesis block
+	storage.PutBlock(genesisBlock)
+	storage.PutState(genesisBlock.StateRoot, stateTrie)
+
+	return &Blockchain{
+		Chain:      []Block{genesisBlock},
+		StateTrie:  stateTrie,
+		PendingTxs: []Transaction{},
+		Validators: []common.Address{},
+		storage:    storage,
 	}
 }
