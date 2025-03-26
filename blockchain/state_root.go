b309bcf1c2b3324ea4e042c678b4a1102e341c9e
mynk03 <mayank4code@gmail.com>
2025-03-26 23:56:25 +0530

    test: Add StateRootTestSuite for block processing validation

--- blockchain/state_root.go
+++ blockchain/state_root.go
@@ -9,15 +9,21 @@ import (
 // ProcessBlock applies transactions to the state trie.
 // ProcessBlock updates the state trie with transactions from a block.
 func ProcessBlock(block Block, trie *state.MptTrie) {
-
 	for _, tx := range block.Transactions {
 		sender, err := trie.GetAccount(tx.From)
 		if err != nil {
 			logrus.WithError(err).Error("Error in Retreiving sender account")
+			continue // Skip this transaction if sender account doesn't exist
 		}
 		receiver, err := trie.GetAccount(tx.To)
 		if err != nil {
-			logrus.WithError(err).Error("Error Retreiving receiver account")
+			logrus.WithError(err).Error("Error in Retreiving receiver account")
+			continue // Skip this transaction if receiver account doesn't exist
+		}
+
+		// Skip if either account is nil
+		if sender == nil || receiver == nil {
+			continue
 		}
 
 		// Update balances and nonce
